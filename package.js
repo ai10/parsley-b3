48272016a73e1e787aa445d8b691db31deeeb160
Chuck Short <sc@binpub.com>
2014-03-27 08:53:08 -0400

    updated parsley from guillaumepotier

--- package.js
+++ package.js
@@ -4,5 +4,5 @@ Package.describe({
 
 Package.on_use(function (api){
     api.use('jquery');
-	api.add_files(['parsley.min.js', 'parsley.extend.min.js'], 'client');
+	api.add_files(['parsley.min.js', 'parsley.remote.min.js'], 'client');
 });
